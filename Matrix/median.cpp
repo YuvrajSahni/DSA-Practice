f425d3808225078db559e9d0cf67fae5827bd065
Yuvraj Sahni <yuvrajsahni3@gmail.com>
2024-02-19 04:45:44 +0530

    matrix questions updated

--- Matrix/median.cpp
+++ Matrix/median.cpp
@@ -1,49 +1,49 @@
-// #include <iostream>
-// #include <stdlib.h>
-// using namespace std;
+#include <iostream>
+#include <stdlib.h>
+#include <math.h>
+using namespace std;
 
-// const int R=3;
-// const int C=5;
+const int R=3;
+const int C=5;
 
-// void median(int arr[][INT32_MAX])
-// {
-//     int min=arr[0][0];
-//     int max=arr[0][C-1];
-//     for(int i=1;i<R;i++)
-//     {
-//         if(arr[i][0])<min)
-//         {
-//             min=arr[i][0];
-//         }
-//         if(arr[i][C-1]>max)
-//         {
-//             max=arr[i][C-1];
-//         }
-//     }
-//     int med_pos=(R*C+1)/2;
-//     while(min<max)
-//     {
-//         int mid = (min+max)/2;
-//         int mid_pos=0;
-//         for(int i=0;i<R;i++)
-//         {
-//             mid_pos+=upper_bound(arr[i],arr[i]+C,mid) - m[i];
-//         }
-//         if(mid_pos<med_pos)
-//         {
-//            min=mid+1;
-//        }
-//         else
-//         {
-//         max=mid
-//         }
-//         return min;
-//     }
+int median(int arr[][INT32_MAX])
+{
+    int mini=arr[0][0];
+    int maxi=arr[0][C-1];
+    for(int i=1;i<R;i++)
+    {
+        if(arr[i][0]<mini)
+        {
+            mini=arr[i][0];
+        }
+        if(arr[i][C-1]>maxi)
+        {
+            maxi=arr[i][C-1];
+        }
+    }
+    int med_pos=(R*C+1)/2;
+    while(mini<maxi)
+    {
+        int mid = (mini+maxi)/2;
+        int mid_pos=0;
+        for(int i=0;i<R;i++)
+        {
+            //mid_pos+=max(arr[i],mid) - arr[i];
+        }
+        if(mid_pos<med_pos)
+        {
+           mini=mid+1;
+       }
+        else
+        {
+        maxi=mid;
+        }
+    }
+    return mini;
+}
 
-// }
-
-// int main()
-// {
-//     cout<<"HELLO";
-//     return 0;
-// }
+int main()
+{
+    cout<<"HELLO";
+    return 0;
+}
